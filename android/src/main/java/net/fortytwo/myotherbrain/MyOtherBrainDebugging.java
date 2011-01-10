10d772bcde7306de9b3f47c8226c3b2c660a0a4a
Joshua Shinavier <josh@fortytwo.net>
2011-01-10 15:56:35 +0800

    Fleshed out a very basic application, including MyOtherBrain icons and some simple UI in the main activity.

--- android/src/main/java/net/fortytwo/myotherbrain/MyOtherBrainDebugging.java
+++ android/src/main/java/net/fortytwo/myotherbrain/MyOtherBrainDebugging.java
@@ -10,6 +10,6 @@ public class MyOtherBrainDebugging extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.debugging);
     }
 }
