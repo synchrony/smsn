eca0bb93f1d47a6bf00edcb2f3bfd0183f1cadc1
Joshua Shinavier <josh@fortytwo.net>
2012-02-20 19:22:44 -0500

    Deleted the rest of the old Elmo- and Flash-based MyOtherBrain. Lots of good code here, but it will never be used again; moving to the TinkerPop stack made things so much simpler.

--- server/src/main/java/net/fortytwo/myotherbrain/flashmob/update/actions/ActionBean.java
+++
@@ -1,9 +0,0 @@
-package net.fortytwo.myotherbrain.flashmob.update.actions;
-
-/**
- * Author: josh
- * Date: Jul 11, 2009
- * Time: 2:10:10 PM
- */
-public abstract class ActionBean {
-}
--- server/src/main/java/net/fortytwo/myotherbrain/flashmob/update/actions/BreakAssociationBean.java
+++
@@ -1,9 +0,0 @@
-package net.fortytwo.myotherbrain.flashmob.update.actions;
-
-/**
- * Author: josh
- * Date: Jul 11, 2009
- * Time: 2:13:48 PM
- */
-public class BreakAssociationBean extends ResourceActionBean {
-}
