b016f5fc5cec0851df918bc80d29957c1704d847
Joshua Shinavier <josh@fortytwo.net>
2013-11-25 16:12:49 -0500

    Using AudioTrack/streaming rather than ToneGenerator cuts a whopping 300ms of latency

--- extendo-android/src/main/java/net/fortytwo/extendo/brainstem/Brainstem.java
+++ extendo-android/src/main/java/net/fortytwo/extendo/brainstem/Brainstem.java
@@ -4,8 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioFormat;
 import android.media.AudioManager;
-import android.media.ToneGenerator;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.util.Log;
 import android.widget.EditText;
 import at.abraxas.amarino.Amarino;
@@ -263,15 +265,57 @@ public class Brainstem {
 
     public class NotificationToneGenerator {
         // Note: is it possible to generate a tone with lower latency than this default generator's?
-        private final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+        //private final ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+
+        private final AudioTrack audioTrack;
+        private final int minSize;
+        private final float synth_frequency = 880;
+        private final int sampleRate;
+
+        public NotificationToneGenerator() {
+            sampleRate = getValidSampleRate();
+
+            minSize = AudioTrack.getMinBufferSize(sampleRate,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT);
+            audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                    sampleRate,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    minSize,
+                    AudioTrack.MODE_STREAM);
+        }
 
         public void play() {
             //startActivity(new Intent(thisActivity, PlaySound.class));
 
-            tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+            //tg.startTone(ToneGenerator.TONE_PROP_BEEP);
 
-            //Instrumentation m_Instrumentation = new Instrumentation();
-            //m_Instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_B);
+            audioTrack.play();
+            short[] buffer = new short[minSize];
+            float angle = 0;
+            //while (true) {
+            //    if (play) {
+            for (int i = 0; i < buffer.length; i++) {
+                float angular_frequency =
+                        (float) (2 * Math.PI) * synth_frequency / sampleRate;
+                buffer[i] = (short) (Short.MAX_VALUE * ((float) Math.sin(angle)));
+                angle += angular_frequency;
+            }
+            audioTrack.write(buffer, 0, buffer.length);
+            //    }
+            //}
+        }
+
+        private int getValidSampleRate() {
+            for (int rate : new int[]{8000, 11025, 16000, 22050, 44100}) {  // add the rates you wish to check against
+                int bufferSize = AudioRecord.getMinBufferSize(rate, AudioFormat.CHANNEL_CONFIGURATION_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
+                if (bufferSize > 0) {
+                    return rate;
+                }
+            }
+
+            throw new IllegalStateException("could not find a valid sample rate for audio output");
         }
     }
 
