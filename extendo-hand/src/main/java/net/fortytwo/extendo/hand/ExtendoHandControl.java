20f0dbe1210a600cb69239f2ed80f5339770cd50
Joshua Shinavier <josh@fortytwo.net>
2015-04-25 13:26:24 -0400

    Added 'multicue' message

--- extendo-hand/src/main/java/net/fortytwo/extendo/hand/ExtendoHandControl.java
+++ extendo-hand/src/main/java/net/fortytwo/extendo/hand/ExtendoHandControl.java
@@ -1,5 +1,6 @@
 package net.fortytwo.extendo.hand;
 
+import com.illposed.osc.OSCMessage;
 import net.fortytwo.extendo.ExtendoDeviceControl;
 import net.fortytwo.extendo.p2p.ExtendoAgent;
 import net.fortytwo.extendo.p2p.osc.OscReceiver;
@@ -14,10 +15,46 @@ import java.util.logging.Logger;
 public class ExtendoHandControl extends ExtendoDeviceControl {
     protected static final Logger logger = Logger.getLogger(ExtendoHandControl.class.getName());
 
+    private static final int
+            MAX_FREQUENCY = 20000, // 20kHz
+            MAX_DURATION = 60000; // 60s
+
+    // outbound paths: for messages to the device
+    private static final String
+            OSC_ALERT = "/alert",
+            OSC_MULTI = "/multi";
+
     public ExtendoHandControl(final OscReceiver oscReceiver,
                               final ExtendoAgent agent) throws DeviceInitializationException {
         super("/exo/hand", oscReceiver, agent);
     }
 
+    public void sendAlertMessage() {
+        OSCMessage message = new OSCMessage(absoluteAddress(OSC_ALERT));
+        send(message);
+    }
 
+    public void sendMulticueMessage(final int toneFrequency,
+                                    final int toneDurationMs,
+                                    final int color,
+                                    final int vibrationDurationMs) {
+        if (toneFrequency < 0 || toneFrequency > 20000) {
+            throw new IllegalArgumentException("tone frequency is out of range: " + vibrationDurationMs);
+        }
+
+        if (toneDurationMs < 0 || toneDurationMs > 60000) {
+            throw new IllegalArgumentException("tone duration is out of range: " + toneDurationMs);
+        }
+
+        if (vibrationDurationMs < 0 || vibrationDurationMs > 60000) {
+            throw new IllegalArgumentException("vibration duration is out of range: " + vibrationDurationMs);
+        }
+
+        OSCMessage m = new OSCMessage(absoluteAddress(OSC_MULTI));
+        m.addArgument(toneFrequency);
+        m.addArgument(toneDurationMs);
+        m.addArgument(color);
+        m.addArgument(vibrationDurationMs);
+        send(m);
+    }
 }
