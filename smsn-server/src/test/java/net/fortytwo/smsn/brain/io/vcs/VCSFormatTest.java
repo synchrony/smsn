142a634416c688b9eb8e501c5be8a9a0ddcf7411
Joshua Shinavier <josh@fortytwo.net>
2017-03-08 19:17:34 -0800

    Test tweak

--- smsn-server/src/test/java/net/fortytwo/smsn/brain/io/vcs/VCSFormatTest.java
+++ smsn-server/src/test/java/net/fortytwo/smsn/brain/io/vcs/VCSFormatTest.java
@@ -10,20 +10,26 @@ import static org.junit.Assert.assertTrue;
 public class VCSFormatTest {
     @Test
     public void atomFilesAccepted() throws Exception {
-        assertTrue(VCSFormat.isAtomFile(new File("a1234567")));
-        assertTrue(VCSFormat.isAtomFile(new File("a12345")));
-        assertTrue(VCSFormat.isAtomFile(new File("au-lH78_")));
+        assertTrue(VCSFormat.isAtomFile(new File("UuYPT6ck7jBL6abZ")));
+        assertTrue(VCSFormat.isAtomFile(new File("0KOr3InLzs3sgAEK")));
+
+        assertTrue(VCSFormat.isAtomFile(new File("UuYPT")));
         File dir = new File("/tmp");
-        assertTrue(VCSFormat.isAtomFile(new File(dir, "a1234567")));
+        assertTrue(VCSFormat.isAtomFile(new File(dir, "UuYPT")));
     }
 
     @Test
     public void nonAtomFilesRejected() throws Exception {
-        assertFalse(VCSFormat.isAtomFile(new File("a1234")));
-        assertFalse(VCSFormat.isAtomFile(new File("1234567")));
-        assertFalse(VCSFormat.isAtomFile(new File("12345678")));
-        assertFalse(VCSFormat.isAtomFile(new File("a 1234567")));
-        assertFalse(VCSFormat.isAtomFile(new File("b1234567")));
+        assertFalse(VCSFormat.isAtomFile(new File("1234")));
+        assertFalse(VCSFormat.isAtomFile(new File("?1234567")));
+        assertFalse(VCSFormat.isAtomFile(new File("12345678 9")));
         assertFalse(VCSFormat.isAtomFile(new File("a1234567.txt")));
+    }
+
+    @Test
+    public void oldSchoolAtomFilesRejected() {
+        assertFalse(VCSFormat.isAtomFile(new File("au-lH78_")));
+        File dir = new File("/tmp");
+        assertFalse(VCSFormat.isAtomFile(new File(dir, "a1_234567")));
     }
 }
