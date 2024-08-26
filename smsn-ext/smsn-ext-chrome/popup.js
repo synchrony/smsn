26e182a8a3f2c4f6df109d10162097645fb67bd3
Joshua Shinavier <josh@fortytwo.net>
2024-08-26 14:53:05 -0700

    Update Chrome extension for Manifest V3 compatibility

--- smsn-ext/smsn-ext-chrome/popup.js
+++ smsn-ext/smsn-ext-chrome/popup.js
@@ -1,21 +1,12 @@
 function copyTabToClipboard(tab, cachingId) {
     var text = "";
 
-    if (0 == tab.url.indexOf("http://en.wikipedia.org/wiki/") || 0 == tab.url.indexOf("https://en.wikipedia.org/wiki/")) {
-        var title = tab.title;
-        var i = title.indexOf(" - ");
-        if (i > 0) {
-            title = title.substring(0, i);
-        }
+    if (tab.url.startsWith("http://en.wikipedia.org/wiki/") || tab.url.startsWith("https://en.wikipedia.org/wiki/")) {
+        var title = tab.title.split(" - ")[0];
 
         var url = "http://dbpedia.org/resource/" + tab.url.substring(tab.url.indexOf("/wiki/") + 6);
-        var j = url.indexOf("#");
-        if (j > 0) {
-            url = url.substring(0, j);
-        }
+        url = url.split("#")[0];
 
-        // Note: the extra indent saves on keystrokes when pasting the text into Brain-mode
-        //       (when it is pasted to an indented position in the tree)
         text += "* " + title + "\n"
             + "                @source universal\n"
             + "                @alias " + url;
@@ -24,15 +15,14 @@ function copyTabToClipboard(tab, cachingId) {
             + "                @alias " + tab.url;
     }
 
-    if (null != cachingId) {
+    if (cachingId) {
         text += "\n    * cached as " + cachingId;
     }
 
     var pre = document.getElementById("clipboard-view");
-    pre.innerHTML = text;
+    pre.textContent = text;
 
     var input = document.getElementById("clipboard-input");
-    // Make the textarea temporarily visible (otherwise, we can't copy)
     input.style.visibility = "visible";
     input.value = text;
     input.focus();
@@ -40,7 +30,7 @@ function copyTabToClipboard(tab, cachingId) {
     document.execCommand("Copy");
     input.style.visibility = "hidden";
 
-    document.getElementById("cache-button").onclick = function() {addToCache(tab)};
+    document.getElementById("cache-button").onclick = function() { addToCache(tab); };
 }
 
 function useTab(tab) {
@@ -49,16 +39,12 @@ function useTab(tab) {
 
 function addToCache(tab) {
     var id = hex_md5(tab.url);
-
-
-
     copyTabToClipboard(tab, id);
 }
 
-// Note: you can't set the body element's "onload" in the HTML, due to Chrome security restrictions.
-//       Neither can you execute a script inline from the HTML.
-onload = function() {
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        useTab(tabs[0]);
+    });
+});
 
-    // Note: chrome.tabs.getCurrent doesn't work from a popup
-    chrome.tabs.getSelected(null, useTab);
-}
