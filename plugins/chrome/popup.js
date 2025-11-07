2a3544d3e48e7e56f51d143e45928c2aaa21816f
Joshua Shinavier <josh@fortytwo.net>
2025-11-06 21:31:34 -0800

    Update the SmSn Chrome plugin

--- plugins/chrome/popup.js
+++ plugins/chrome/popup.js
@@ -1,64 +1,60 @@
-function copyTabToClipboard(tab, cachingId) {
-    var text = "";
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs.length > 0) {
+            let tab = tabs[0];
 
-    if (0 == tab.url.indexOf("http://en.wikipedia.org/wiki/") || 0 == tab.url.indexOf("https://en.wikipedia.org/wiki/")) {
-        var title = tab.title;
-        var i = title.indexOf(" - ");
-        if (i > 0) {
-            title = title.substring(0, i);
+            document.getElementById("copy-button").addEventListener("click", () => {
+                copyTabToClipboard(tab, null);
+            });
+
+            document.getElementById("cache-button").addEventListener("click", () => {
+                addToCache(tab);
+            });
         }
+    });
+});
 
-        var url = "http://dbpedia.org/resource/" + tab.url.substring(tab.url.indexOf("/wiki/") + 6);
-        var j = url.indexOf("#");
+function copyTabToClipboard(tab, cachingId) {
+    let text = formatTabText(tab, cachingId);
+
+    navigator.clipboard.writeText(text).then(() => {
+        console.log("Copied to clipboard successfully!");
+    }).catch(err => {
+        console.error("Error copying to clipboard:", err);
+    });
+
+    document.getElementById("clipboard-view").innerHTML = text;
+}
+
+function formatTabText(tab, cachingId) {
+    let text = "";
+
+    if (tab.url.startsWith("http://en.wikipedia.org/wiki/") || tab.url.startsWith("https://en.wikipedia.org/wiki/")) {
+        let title = tab.title.split(" - ")[0];
+        let url = "http://dbpedia.org/resource/" + tab.url.substring(tab.url.indexOf("/wiki/") + 6);
+        let j = url.indexOf("#");
         if (j > 0) {
             url = url.substring(0, j);
         }
 
-        // Note: the extra indent saves on keystrokes when pasting the text into Brain-mode
-        //       (when it is pasted to an indented position in the tree)
-        text += "* " + title + "\n"
-            + "                @source universal\n"
-            + "                @alias " + url;
+        text += `* ${title}\n                @source universal\n                @alias ${url}`;
     } else {
-        text += "* " + tab.title + " (web page)\n"
-            + "                @alias " + tab.url;
+        text += `* ${tab.title} (web page)\n                @alias ${tab.url}`;
     }
 
-    if (null != cachingId) {
-        text += "\n    * cached as " + cachingId;
+    if (cachingId) {
+        text += `\n    * cached as ${cachingId}`;
     }
 
-    var pre = document.getElementById("clipboard-view");
-    pre.innerHTML = text;
-
-    var input = document.getElementById("clipboard-input");
-    // Make the textarea temporarily visible (otherwise, we can't copy)
-    input.style.visibility = "visible";
-    input.value = text;
-    input.focus();
-    input.select();
-    document.execCommand("Copy");
-    input.style.visibility = "hidden";
-
-    document.getElementById("cache-button").onclick = function() {addToCache(tab)};
-}
-
-function useTab(tab) {
-    copyTabToClipboard(tab, null);
+    return text;
 }
 
 function addToCache(tab) {
-    var id = hex_md5(tab.url);
-
-
-
-    copyTabToClipboard(tab, id);
+    crypto.subtle.digest("SHA-256", new TextEncoder().encode(tab.url))
+        .then(hashBuffer => {
+            let hashArray = Array.from(new Uint8Array(hashBuffer));
+            let id = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+            copyTabToClipboard(tab, id);
+        });
 }
 
-// Note: you can't set the body element's "onload" in the HTML, due to Chrome security restrictions.
-//       Neither can you execute a script inline from the HTML.
-onload = function() {
-
-    // Note: chrome.tabs.getCurrent doesn't work from a popup
-    chrome.tabs.getSelected(null, useTab);
-}
